11a51d73a4cf02c1e4e38c0980fc8919902b7095
msdb198901 <meisheng@songkegame.com>
2026-05-18 17:40:16 +0800

    提交代码

---
+++ Components/AnimationState.cpp
@@ -0,0 +1,44 @@
+﻿#include "AnimationState.h"
+#include "Sprite.h"
+#include "AssetPool.h"
+
+Sprite* AnimationState::defaultSprite = new Sprite();
+
+void AnimationState::refreshTexture() {
+    for (Frame& frame : animationFrames) {
+        frame.sprite->setTexture(AssetPool::getTexture(frame.sprite->getTexture()->getFilepath()));
+    }
+}
+
+void AnimationState::addFrame(Sprite* sprite, float duration) {
+    animationFrames.push_back({sprite, duration});
+}
+
+void AnimationState::addFrames(std::vector<Sprite*> sprites, float duration) {
+    for (Sprite* sprite : sprites) {
+        addFrame(sprite, duration);
+    }
+}
+
+void AnimationState::setLoop(bool loop) {
+    this->doesLoop = loop;
+}
+
+void AnimationState::update(float deltaTime) {
+   if (currentSpriteIndex < (int)animationFrames.size()) {
+       time -= deltaTime;
+       if (time <= 0) {
+           if (!(currentSpriteIndex == animationFrames.size() - 1 && !doesLoop)) {
+               currentSpriteIndex = (currentSpriteIndex + 1) % animationFrames.size();
+           }
+           time = animationFrames[currentSpriteIndex].frameTime;
+       }
+   }
+}
+
+Sprite* AnimationState::getCurrentSprite() {
+    if (currentSpriteIndex < 0 || currentSpriteIndex >= (int)animationFrames.size()) {
+        return defaultSprite;
+    }
+    return animationFrames[currentSpriteIndex].sprite;
+}
---
+++ Components/AnimationState.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+
+class Sprite;
+
+struct Frame {
+    Sprite* sprite;
+    float frameTime;
+}  ;
+
+class AnimationState {
+    public:
+        void refreshTexture();
+        void addFrame(Sprite* sprite, float frameTime);
+        void addFrames(std::vector<Sprite*> sprites, float frameTime);
+        void setLoop(bool doesLoop);
+
+        void update(float dt);
+        Sprite* getCurrentSprite();
+
+    public:
+        std::string title;
+        std::vector<Frame> animationFrames;
+
+    private:
+        static Sprite* defaultSprite;
+        float time = 0.0f;
+        int currentSpriteIndex = 0;
+        bool doesLoop = false;
+};
